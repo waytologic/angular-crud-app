e9b36715fcff7b37693dc06273a6863779660345
Way2logic <128026796+waytologic@users.noreply.github.com>
2023-08-30 19:57:43 +0530

    Update employee.service.ts

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -7,20 +7,24 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
   constructor(private _http: HttpClient) {}
-
+  const BaseURL ="https://my-json-server.typicode.com/waytologic/arpino/";
   addEmployee(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/employees', data);
+   // return this._http.post('http://localhost:3000/employees', data);
+     return this._http.post(BaseURL+'/employees', data);
   }
 
   updateEmployee(id: number, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/employees/${id}`, data);
+   // return this._http.put(`http://localhost:3000/employees/${id}`, data);
+     return this._http.put(BaseURL+`/employees/${id}`, data);
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('http://localhost:3000/employees');
+    //return this._http.get('http://localhost:3000/employees');
+     return this._http.get(BaseURL+'/employees');
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/employees/${id}`);
+   // return this._http.delete(`http://localhost:3000/employees/${id}`);
+     return this._http.delete(BaseURL+`/employees/${id}`);
   }
 }
